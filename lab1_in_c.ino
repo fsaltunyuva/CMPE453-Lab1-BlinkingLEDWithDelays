4ba648440326a7a61cef470afd082c41c0bc4d66
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2024-10-11 10:16:48 +0300

    Comment Lines

--- lab1_in_c.ino
+++ lab1_in_c.ino
@@ -1,27 +1,35 @@
-#include <avr/io.h> 
-#include <util/delay.h>
+#include <avr/io.h> // Header file for the AVR Microcontroller.
+#include <util/delay.h> // Header file for the delay function.
 
 int main(void) {
 
-DDRD = 0b01001000;
-DDRB = 0b00000010; 
+  DDRD = 0b01001000; // Set the data direction register D to output on necessary pins.
+  DDRB = 0b00000010; // Set the data direction register B to output on necessary pins.
 
-while (1) {
+  while (1) { // Infinite loop to keep the program running.
+
+    // ON the all LEDs together and wait for 3000ms  
     PORTD = 0b01001000; 
     PORTB = 0b00000010;
     _delay_ms(3000);
 
+    // OFF the red and yellow LEDs and ON the blue LED together and wait for 4000ms
     PORTD = 0b01000000; 
     PORTB = 0b00000000;
     _delay_ms(4000);
 
+    // OFF the blue LED and ON the yellow and red LEDs together and wait for 5000ms
     PORTD = 0b00001000; 
     PORTB = 0b00000010;
     _delay_ms(5000);
 
+    // OFF the all LEDs and wait for 2000ms
     PORTD = 0b00000000; 
     PORTB = 0b00000000;
     _delay_ms(2000);
+
+    // Return to the beginning
   } 
+  
   return 0; 
 }
--- lab1_in_ino.ino
+++ lab1_in_ino.ino
@@ -1,33 +1,40 @@
-int redPin = 3;                  
-int bluePin = 6;  
-int yellowPin = 9;               
+int redPin = 3; // Red LED connected to digital pin 3              
+int bluePin = 6; // Blue LED connected to digital pin 6
+int yellowPin = 9; // Yellow LED connected to digital pin 9               
 
-void setup()                      
+void setup() // Run once, when the sketch starts                      
 {
-  pinMode(redPin, OUTPUT);       
+  // Setting the digital pins as output
+  pinMode(redPin, OUTPUT);    
   pinMode(bluePin, OUTPUT);      
   pinMode(yellowPin, OUTPUT); 
 }
 
-void loop()                      
+void loop() // Run over and over again                      
 {
+  // ON the all LEDs together and wait for 3000ms
   digitalWrite(redPin, HIGH);   
   digitalWrite(bluePin, HIGH);
   digitalWrite(yellowPin, HIGH);  
   delay(3000); 
 
+  // OFF the red and yellow LEDs and ON the blue LED together and wait for 4000ms
   digitalWrite(redPin, LOW);     
   digitalWrite(yellowPin, LOW); 
   digitalWrite(bluePin, HIGH);
   delay(4000); 
 
+  // OFF the blue LED and ON the yellow and red LEDs together and wait for 5000ms
   digitalWrite(redPin, HIGH);     
   digitalWrite(yellowPin, HIGH); 
   digitalWrite(bluePin, LOW);
   delay(5000); 
 
+  // OFF the all LEDs and wait for 2000ms
   digitalWrite(redPin, LOW);     
   digitalWrite(yellowPin, LOW); 
   digitalWrite(bluePin, LOW);
-  delay(2000);                    
+  delay(2000);    
+
+  // Return to the beginning                
 }
